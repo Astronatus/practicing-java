a0ff51d53aaee4e570974dd04c6ff5dac59b85d6
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2025-03-24 22:15:14 -0500

    feat: add overwritten variables

--- module-2/mathematical-operators/MathOperators.java
+++ module-2/mathematical-operators/MathOperators.java
@@ -1,8 +1,15 @@
 public class MathOperators {
     static int x;
+    static int y;
+    static int z;
     
     public static void main(String[] args) {
-        x = 4 + 3 * 2;    
+        x = 2;    
+        y = x * 3;
+        int z = y / 2;
+
+        x = (2 + z) % 2;
+        
         System.out.println("x = " + x);
     }
 }
