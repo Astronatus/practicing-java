43eb90c284012a67415bf49708c4cfd79ddea994
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2025-03-24 22:31:14 -0500

    feat: add operations

---
+++ module-2/mathematical-operators/ExerciseA.java
@@ -0,0 +1,9 @@
+public class ExerciseA {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 3;
+        int c = 4;
+        c = a + b;
+        System.out.println(c);
+    }
+}
---
+++ module-2/mathematical-operators/ExerciseB.java
@@ -0,0 +1,8 @@
+public class ExerciseB {
+    public static void main (String[] args) {
+        int a = 3;
+        int b = 1;
+        int c = 2 + a * 5 - b;
+        System.out.println(c);
+    }
+}
