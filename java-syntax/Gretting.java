6ca63261854661fe9ced3b9635aade53e5a0785a
Antonioz <1317580+Astronatus@users.noreply.github.com>
2025-06-18 21:27:17 -0500

    feat: add static variable
    feat: add static variables
    
    feat: reorder directories

---
+++ java-syntax/Gretting.java
@@ -0,0 +1,7 @@
+class Gretting {
+
+    public static void main(String[] args) {
+        System.out.println("Java");
+    }
+    
+}
---
+++ java-syntax/Message.java
@@ -0,0 +1,10 @@
+class Message {
+    static String message;
+    static int age;
+    public static void main(String[] args) {
+        message = "Hello!";
+        age = 25;
+        System.out.println(message);
+        System.out.println(age);
+    }
+}
