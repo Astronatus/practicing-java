c2f252f1d0e3e10edcdc016e8d326f02bc731348
Antonioz <1317580+Astronatus@users.noreply.github.com>
2025-07-26 21:58:45 -0500

    feat: change project structure and add array examples

---
+++ 2_working_with_variables/2.4_arrays/NumberArray.java
@@ -0,0 +1,6 @@
+class NumberArray {
+    public static void main (String[] args) {
+        int[] prices = {20, 30, 40, 50, 60};
+        System.out.println(prices);
+    }
+}
---
+++ 2_working_with_variables/2.4_arrays/StringArray.java
@@ -0,0 +1,13 @@
+// All values in array must have the same type
+class StringArray {
+    public static void main (String[] args) {
+        String[] productNames = {
+            "Laptop",
+            "Smartphone",
+            "Tablet",
+            "Smartwatch"
+        };
+
+        System.out.println(productNames[0]);
+    }
+}
